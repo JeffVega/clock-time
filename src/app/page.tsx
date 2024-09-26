05571db174c65e027e02827abebfc622bd24a986
JeffVega <jeff.vega321@gmail.com>
2024-09-26 09:18:27 -0700

    * updated the time zone meta Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 
 const TimeZone = dynamic(() => import("@/_components/TimeZone/Timezone"), { ssr: false });
+
 export default function Home() {
   return (
     <div>
