fbef788ec672b851676bcf052e395afec6f51aa1
JeffVega <jeff.vega321@gmail.com>
2024-11-21 23:34:21 -0800

    refactor layout component to improve readability and add clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,40 +6,48 @@ import Script from "next/script";
 import Footer from "@/_components/Footer";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "TimeSync World: Manage & Convert Global Time Zones Easily",
-  description: "Convert, add, and manage time zones. Perfect for professionals, expats, and travelers to simplify scheduling and organize international meetings",
-  alternates: {
-    canonical: './',
-},
+	title: "TimeSync World: Manage & Convert Global Time Zones Easily",
+	description:
+		"Convert, add, and manage time zones. Perfect for professionals, expats, and travelers to simplify scheduling and organize international meetings",
+	alternates: {
+		canonical: "./",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable}  `}
-      >
-        <Toaster position="top-center" />
-        {children}
-        <Footer />
-      </body>
-     
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable}  `}>
+				<Script id="surf">
+					{`<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "o9cslzposx");
+</script>`}
+				</Script>
+				<Toaster position="top-center" />
+				{children}
+				<Footer />
+			</body>
+
+			<Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+		</html>
+	);
 }
