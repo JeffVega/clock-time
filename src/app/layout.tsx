19befd821afaf5e3390ab35ce8339f0244558265
JeffVega <jeff.vega321@gmail.com>
2024-11-27 14:15:22 -0800

    update clarity tracking script to use environment variable and streamline script loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,18 +36,20 @@ export default function RootLayout({
 				<Toaster position="top-center" />
 				{children}
 				<Footer />
-				<Script id="surf" strategy="afterInteractive">
+				<Script id="clarity-script" strategy="afterInteractive">
 					{`
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "o9cslzposx");
+    })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID});
 `}
 				</Script>
+				<Script
+					strategy="afterInteractive"
+					src="https://scripts.simpleanalyticscdn.com/latest.js"
+				/>
 			</body>
-
-			<Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
 		</html>
 	);
 }
