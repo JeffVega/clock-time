c663a6b95dcdb1a3027b93aadf3417c558d300fb
JeffVega <jeff.vega321@gmail.com>
2024-12-15 13:28:08 -0800

    refactor TimeZoneWrapper: update import statement for AbstractIntlMessages type and improve code formatting for better readability

--- src/components/TimeZone/TimeZoneWrapper.tsx
+++ src/components/TimeZone/TimeZoneWrapper.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
+import { type AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import TimeZoneDisplay from "./Timezone";
 
 type Props = {
-  messages: AbstractIntlMessages; // Specify messages as a record of string key-value pairs
-  locale: string;
+	messages: AbstractIntlMessages; // Specify messages as a record of string key-value pairs
+	locale: string;
 };
 
 export default function TimeZoneWrapper({ messages, locale }: Props) {
-  return (
-    <NextIntlClientProvider messages={messages} locale={locale}>
-      <TimeZoneDisplay />
-    </NextIntlClientProvider>
-  );
+	return (
+		<NextIntlClientProvider messages={messages} locale={locale}>
+			<TimeZoneDisplay />
+		</NextIntlClientProvider>
+	);
 }
